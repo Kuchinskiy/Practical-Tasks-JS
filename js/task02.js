7ce128d69667dcf6057da12a3d04b8149e5766c9
Sergey <domarch388640@gmail.com>
2020-06-08 11:57:50 +0300

    function practice

--- js/task02.js
+++ js/task02.js
@@ -145,19 +145,22 @@
 
 // !!! Function Declaration
 
-// function areArraysSame(arr, array) {
-//   if (arr.length !== array.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] !== array[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// console.log(areArraysSame([1, 2, 3], [4, 5, 6])); // false
-// console.log(areArraysSame([1, 2, 3], [1, 2, 3])); // true
-// console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4])); // false
+function areArraysSame(arr, array) {
 
+  if (arr.length !== array.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== array[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(areArraysSame([1, 2, 3], [4, 5, 6])); // false
+console.log(areArraysSame([1, 2, 3], [1, 2, 3])); // true
+console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4])); // false
 
