3abab60136e58d0fb5672838c9e72c7872656889
Sergey <domarch388640@gmail.com>
2020-09-06 18:00:20 +0300

    commit

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -13,7 +13,7 @@ let guesses = 0; // определяет подсчитывая, сколько 
 let isSunk = false; // определяет и сохраняет инфу о том уничтожена цель или нет, будет изменяться на 'true';
 
 // Создаем цикл для запуска игры;
-while (isSunk === false) {
+while (isSunk == false) {
   guess = prompt("Ready, aim, fire! (enter a number 0-6):");
 
   if (guess < 0 || guess > 6) {
@@ -21,11 +21,11 @@ while (isSunk === false) {
   } else {
     guesses += 1;
 
-    if (guess === location1 || guess === location2 || guess === location3) {
+    if (guess == location1 || guess == location2 || guess == location3) {
       alert("HIT!");
       hits += 1;
 
-      if (hits === 3) {
+      if (hits == 3) {
         isSunk = true;
         alert("You sank my battleship!");
       }
@@ -39,35 +39,5 @@ let stats = `You took ${guesses} guesses to sink the battleship which means your
   3 / guesses
 }`;
 
-alert(stats);
-
-
-// Test
-// while (isSunk == false) {
-
-//   guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-
-//   if (guess < 0 || guess > 6) {
-//     alert("Please enter a valid cell number!");
-//   } else {
-//     guesses = guesses + 1;
-//     if (guess == location1 || guess == location2 || guess == location3) {
-//       alert("HIT!");
-//       hits = hits + 1;
-//       if (hits == 3) {
-//         isSunk = true;
-//         alert("You sank my battleship!");
-//       }
-//     } else {
-//       alert("MISS");
-//     }
-//   }
-// }
-// let stats =
-//   "You took " +
-//   guesses +
-//   " guesses to sink the battleship, " +
-//   "which means your shooting accuracy was " +
-//   3 / guesses;
-
 // alert(stats);
+
