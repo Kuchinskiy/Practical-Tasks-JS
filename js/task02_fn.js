bd3fd5f5355bebf23887f4760a2e4d8ef34b5512
Sergey <domarch388640@gmail.com>
2020-09-11 09:10:15 +0300

    commit

--- js/task02_fn.js
+++ js/task02_fn.js
@@ -165,3 +165,16 @@
 // console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4])); // false
 
 
+function whatShallWear (temp) {
+  if (temp < 60) {
+    console.log('Wear a jacket');
+  } else if (temp < 70) {
+    console.log('Wear a sweater');
+  } else {
+    console.log('Wear t-shirt');
+  }
+}
+
+whatShallWear(50);
+whatShallWear(80);
+whatShallWear(60);
